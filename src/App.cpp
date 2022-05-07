4c02169f7be1c8a8a54e1768fdcc44d3a9cd1b06
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2022-05-07 18:49:48 +0200

    test for mac

--- src/App.cpp
+++ src/App.cpp
@@ -79,7 +79,8 @@ void App::Update() {
 void App::Render() {
     // Couleur du fond sans rien (ici gris)
     glClearColor(0.3f, 0.3f, 0.3f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     
     glMatrixMode(GL_MODELVIEW);
 
